d9c24e59155a7a2706934404a7c1f685ed745227
Ebenezer <ebenezerrao3@gmail.com>
2025-01-07 14:53:22 +0530

    Updated

--- javascript/script.js
+++ javascript/script.js
@@ -2,16 +2,22 @@ const btn = document.getElementById("btn");
 const container = document.getElementById("container");
 
 async function clickHandler() {
-	const data = await fetch("https://jsonplaceholder.typicode.com/posts/1")
-	const dataToJson = await data.json()
-	// console.log(dataToJson)
-	container.innerHTML = JSON.stringify(dataToJson)
+    const data = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+    const dataToJson = await data.json();
+    container.innerHTML = "";
+
+    for (const key in dataToJson) {
+        if (dataToJson.hasOwnProperty(key)) {
+            const item = document.createElement("p");
+            item.innerHTML = `<strong>${key}:</strong> ${dataToJson[key]}`;
+            container.appendChild(item);
+        }
+    }
 }
 
 function mouseOutHandler() {
-	container.innerHTML = "";
+    container.innerHTML = "";
 }
 
 btn.addEventListener("click", clickHandler);
-// btn.addEventListener("mouseover", clickHandler);
-// btn.addEventListener("mouseout", mouseOutHandler);
+
